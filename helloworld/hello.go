52743f1d724db22bd31519a1c23925d0d3b70684
Marco Santarcangelo Zazzetta <marcosantar93@gmail.com>
2020-06-28 15:42:44 -0300

    var keyword

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+// x:=10 can't do
+var y = 11
+var z int32
+
 func main() {
-	fmt.Println("hello world")
+	n, err := fmt.Println("hello world")
+	fmt.Println(n)
+	fmt.Println(err)
+	z = 12
+	x := 10
+	fmt.Println(x + y + int(z))
 }
